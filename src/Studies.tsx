8568e2bfa5ff958ba0f8823495d37f18be1521e7
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2023-05-25 22:25:38 -0700

    feat: ubc student

--- src/Studies.tsx
+++ src/Studies.tsx
@@ -7,12 +7,9 @@ function Studies() {
             <h1 className={widgetCss.nobr}>studies</h1>
             <p className={widgetCss.headerBracket}>]</p>
             <p>
-                I'm currently part of <strong>UTP '23</strong>, as well
-                as the <strong>Future Science Leaders: Innovate{" "}
-                (Yr 2)</strong> program. Previously, I was a part of{" "}
-                the <strong>FSL: Discover (Yr 1)</strong> program. I{" "}
-                also hold an <strong>IT Support certification from{" "}
-                Google</strong>.
+				I'm currently a student at <strong>UBC</strong>. I also{" "}
+                hold an{" "}
+                <strong>IT Support certification from Google</strong>.
             </p>
         </div>
     )
